97d0c71d470e394d540e8f7903884b8bfdff90d0
mmartinez150 <35519818+mmartinez150@users.noreply.github.com>
2018-04-30 17:21:20 -0700

    skeleton

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'HQtmZP676sZCJFE5E5kJ2CVM1',
+  consumer_secret: 'CCuj1OuTRdfJwnFkdzkhSqeVUmeGdiSqQlRPInS84ukQZyYeO0',
+  access_token_key: '990697046209253376-afGtAJrztsOxkvqnera6FI6ZPmRa4dC',
+  access_token_secret: 'GZJignZa2mTRqPPkHbncWuSmNcHbzRThHhJtwnLgLiT90',
+}
+
+
